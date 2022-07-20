e91135bfbe2287e835de7579898b8aad6c51ffd9
Rishi <18134@students.mrgs.school.nz>
2022-07-21 00:35:26 +1200

    Main Page

---
+++ app/src/main/java/com/example/a13cscassignment4/entities/Note.java
@@ -0,0 +1,97 @@
+package com.example.a13cscassignment4.entities;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "notes")
+public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @ColumnInfo(name = "date_time")
+    private String dateTime;
+
+    @ColumnInfo(name = "subtitle")
+    private String subtitle;
+
+    @ColumnInfo(name = "note_text")
+    private String note_text;
+
+    @ColumnInfo(name = "image_path")
+    private String imagePath;
+
+    @ColumnInfo(name = "color")
+    private String color;
+
+    @ColumnInfo(name = "web_link")
+    private String weblink;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getNote_text() {
+        return note_text;
+    }
+
+    public void setNote_text(String note_text) {
+        this.note_text = note_text;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getWeblink() {
+        return weblink;
+    }
+
+    public void setWeblink(String weblink) {
+        this.weblink = weblink;
+    }
+}
